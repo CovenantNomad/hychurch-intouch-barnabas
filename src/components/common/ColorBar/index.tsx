972eb2ed35fe15aba8657235a586c02d458e402c
Covenant Nomad <covenantnomad@Covenantui-Macmini.local>
2025-02-10 01:20:36 +0900

    fix: ColorBar 랜덤컬러추가

--- src/components/common/ColorBar/index.tsx
+++ src/components/common/ColorBar/index.tsx
@@ -17,12 +17,17 @@ const bgColors = [
   'bg-pink-500',
 ];
 
+const getStableRandomColor = (index: number) => {
+  const hash = index % bgColors.length; // index를 colors 배열 크기로 나눈 나머지를 사용
+  return bgColors[hash]; // 고유한 색상 반환
+};
+
 const ColorBar = ({ index }: { index: number }) => {
   return (
     <div
       className={cn(
         'w-1 h-[50px] rounded-full mr-2',
-        bgColors[Math.floor(index * bgColors.length)]
+        getStableRandomColor(index)
       )}
     />
   );
